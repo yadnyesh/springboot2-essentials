b1849fff8821bbc13dfa9267691157e28a6db896
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-14 10:45:38 +0530

    Inital Commit

---
+++ src/test/java/yb/yadnyesh/springboot2/essentials/Springboot2EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh.springboot2.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
