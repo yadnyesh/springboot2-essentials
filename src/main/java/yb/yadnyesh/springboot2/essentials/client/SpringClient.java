7a0420d6abf0e902cb9ec58338d02a298e92ef37
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-22 13:02:12 +0530

    RestTemplate Using getForObject

--- src/main/java/yb/yadnyesh/springboot2/essentials/client/SpringClient.java
+++ src/main/java/yb/yadnyesh/springboot2/essentials/client/SpringClient.java
@@ -1,11 +1,15 @@
 package yb.yadnyesh.springboot2.essentials.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import yb.yadnyesh.springboot2.essentials.domain.Anime;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class SpringClient {
@@ -19,8 +23,15 @@ public class SpringClient {
 //                .getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
 //        log.info("Anime name: {}", anime.toString());
 
-        Anime[] animeArray = new RestTemplate()
-                .getForObject("http://localhost:8080/animes/", Anime[].class);
-        log.info("Anime Array: {}", Arrays.toString(animeArray));
+//        Anime[] animeArray = new RestTemplate()
+//                .getForObject("http://localhost:8080/animes/", Anime[].class);
+//        log.info("Anime Array: {}", Arrays.toString(animeArray));
+
+        ResponseEntity<List<Anime>> animeResponseEntity = new RestTemplate()
+                .exchange("http://localhost:8080/animes/", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {
+                });
+
+        log.info("Anime List: {}", animeResponseEntity.getBody());
+
     }
 }
